617eee30ce2876529561a7b69e2a3e8e89d8e48d
John Anthony Labarga <john.labarga@unilever.com>
2023-09-20 14:23:05 -0400

    generator to get rows from mintel

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import hashlib
+import os
+import pyarrow.parquet as pq
 
 
 def make_hash_key(df_row) -> str:
@@ -11,3 +13,29 @@ def make_hash_key(df_row) -> str:
     hash_key = hashlib.md5(row_str.encode()).hexdigest()
 
     return hash_key
+
+
+def get_mintel_row(parquet_files_path):
+    files = [f for f in os.listdir(parquet_files_path) if f.endswith(".parquet")]
+
+    for file in files:
+        filepath = os.path.join(parquet_files_path, file)
+        parquet_file = pq.ParquetFile(filepath)
+
+        # Get the number of row groups
+        num_row_groups = parquet_file.num_row_groups
+
+        # Go through each row group
+        for i in range(num_row_groups):
+            table = parquet_file.read_row_group(i)
+            df = table.to_pandas()
+
+            # Yield each row of the DataFrame.
+            for index, row in df.iterrows():
+                yield row
+
+
+if __name__ == "__main__":
+    for row in get_mintel_row("../data/mintel_source"):
+        print(make_hash_key(row))
+        break
