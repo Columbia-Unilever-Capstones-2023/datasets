e1015f0008fbd5dc9ee684d22ec94e2d77f895fe
John Anthony Labarga <john.labarga@unilever.com>
2023-09-28 10:23:15 -0400

    prevent duplicate downloads

--- src/image.py
+++ src/image.py
@@ -1,11 +1,13 @@
 import requests
 import fnmatch
 import os
+from tqdm import tqdm
+from tabular import get_mintel_row, make_hash_key
 
 
 def _image_already_downloaded(images_dir: str, hash_key: str) -> bool:
     for entry in os.scandir(images_dir):
-        if entry.is_file() and fnmatch.fnmatch(entry.name, f'{hash_key}*'):
+        if entry.is_file() and fnmatch.fnmatch(entry.name, f"{hash_key}*"):
             return True
     return False
 
@@ -16,3 +18,17 @@ def save_image(image_url: str, image_filename: str, save_path: str) -> None:
     open(f"{save_path}/{image_filename}.png", "wb").write(r.content)
 
     return
+
+
+if __name__ == "__main__":
+    for row in tqdm(get_mintel_row("../data/mintel_source")):
+        hash_key = make_hash_key(row)
+
+        if not _image_already_downloaded(images_dir="../data/mintel_source", hash_key=hash_key):
+            all_images = row["ProductAllImagesLinksText"].strip().split()
+            for idx, url in enumerate(all_images):
+                save_image(
+                    image_url=url,
+                    image_filename=f"{hash_key}_{idx}",
+                    save_path="../data/images",
+                )
