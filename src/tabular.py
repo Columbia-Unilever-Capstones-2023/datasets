ab77751f6d252746ba44c639220a3a6a647ea0ab
John Anthony Labarga <john.labarga@unilever.com>
2023-11-07 15:54:39 -0500

    methods to count and generate rows of filtered data

--- src/tabular.py
+++ src/tabular.py
@@ -1,8 +1,12 @@
+import glob
 import hashlib
 import os
+
+import pandas as pd
 import pyarrow.parquet as pq
 import urllib3
 import sqlalchemy
+from typing import Tuple, List
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -34,7 +38,34 @@ def get_parquet_total_row_count(parquet_files_path: str) -> int:
     return total_rows
 
 
-def get_mintel_row(parquet_files_path):
+def get_filtered_mintel_row(
+    parquet_files_path: str, filters: List[List[Tuple]]
+) -> pd.DataFrame:
+    mintel_files = glob.glob(f"{parquet_files_path}/**/*.parquet", recursive=True)
+
+    for file in mintel_files:
+        df = pd.read_parquet(file, filters=filters).drop_duplicates()
+        for index, row in df.iterrows():
+            yield row
+
+
+def get_filtered_parquet_row_count(
+    parquet_files_path: str, filters: List[List[Tuple]]
+) -> int:
+    total = 0
+
+    mintel_files = glob.glob(f"{parquet_files_path}/**/*.parquet", recursive=True)
+
+    for file in mintel_files:
+        df = pd.read_parquet(
+            file, filters=filters, columns=[x[0] for x in filters[0]]
+        )
+        total += df.shape[0]
+
+    return total
+
+
+def get_mintel_row(parquet_files_path: str):
     files = [f for f in os.listdir(parquet_files_path) if f.endswith(".parquet")]
 
     for file in files:
@@ -53,7 +84,8 @@ def get_mintel_row(parquet_files_path):
             for index, row in df.iterrows():
                 yield row
 
-def load_mintel_into_sql(sql_connection_string:str) -> None:
+
+def load_mintel_into_sql(sql_connection_string: str) -> None:
     engine = sqlalchemy.create_engine(sql_connection_string)
 
 
